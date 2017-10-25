0736369c2eb1e30e3b7788e6d303119bdcbcfde8
Adam Hansen <adamlars90@gmail.com>
2017-10-25 09:04:40 +0200

    Added navbar and initial routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {NavigationBarComponent} from './shared/navigation-bar/navigation-bar.component';
+import {appRoutes} from './app.routing';
+import {PageNotFoundComponent} from './shared/page-not-found/page-not-found.component';
+import {HomeComponent} from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    PageNotFoundComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    appRoutes
   ],
   providers: [],
   bootstrap: [AppComponent]
