89cf2ed561c6d38d0127924a63f64335c14451b3
Mathias Sejrup Plougmann <mathias_j93@hotmail.com>
2017-10-30 14:29:07 +0100

    Guild Setup Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import {appRoutes} from './app.routing';
 import {PageNotFoundComponent} from './shared/page-not-found/page-not-found.component';
 import {HomeComponent} from './home/home.component';
 import {VolunteerModule} from './volunteer/volunteer.module';
+import { GuildsComponent } from './guild/guilds/guilds.component';
+import { GuildDetailComponent } from './guild/guilds/guild-detail/guild-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
     PageNotFoundComponent,
-    HomeComponent
+    HomeComponent,
+    GuildsComponent,
+    GuildDetailComponent
   ],
   imports: [
     BrowserModule,
