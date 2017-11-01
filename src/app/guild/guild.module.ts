a58d93dacc8dad9b5c4fb66b0d36b549a217374b
Mathias Sejrup Plougmann <mathias_j93@hotmail.com>
2017-11-01 11:27:45 +0100

    Added HTTP Client

--- src/app/guild/guild.module.ts
+++ src/app/guild/guild.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GuildsComponent } from './guilds/guilds.component';
 import {guildRoutes} from './guild.routing';
+import {HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
   imports: [
     CommonModule,
-    guildRoutes
+    guildRoutes,
+    HttpClientModule
   ],
   declarations: [GuildsComponent]
 })
