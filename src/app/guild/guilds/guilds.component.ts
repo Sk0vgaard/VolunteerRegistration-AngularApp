70e995727f732b4f004b11659370ac99731ee94a
Sk0vgaard <Sk0vgaard@live.dk>
2017-11-06 11:34:49 +0100

    Guild component SETUP

--- src/app/guild/guilds/guilds.component.ts
+++ src/app/guild/guilds/guilds.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Guild} from "../shared/guild.model";
+import {GuildService} from "../shared/guild.service";
 
 @Component({
   selector: 'app-guilds',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GuildsComponent implements OnInit {
 
-  constructor() { }
+  guilds: Guild[];
+  selectGuild: Guild;
+  selectedRow: number;
+  isUpdateClicked = false;
+
+  constructor(private guildService: GuildService) { }
 
   ngOnInit() {
+  this.updateListOfGuilds();
   }
 
+  selectedGuild(guild: Guild, selectedRow: number) {
+    this.selectGuild = guild;
+    this.selectedRow = selectedRow;
+    this.isUpdateClicked = false;
+  }
+
+  setUpdate($event) {
+    this.isUpdateClicked = !this.isUpdateClicked;
+    $event.stopPropagation();
+  }
+
+  updateListOfGuilds() {
+    this.guildService.get()
+      .subscribe(guilds => this.guilds = guilds);
+  }
+
+  // deleteGuild($event, guild: Guild) {
+  //   this.guildService.delete(guild.id)
+  //     .subscribe(() => this.updateListOfGuilds());
+  //   this.selectedGuild = null;
+  //   $event.stopPropagation();
+  // }
 }
