b8427712461cb9b57668a41adeae57e77e0cf6af
Mathias Sejrup Plougmann <mathias_j93@hotmail.com>
2017-11-08 11:20:29 +0100

    Readied GuildCreateComponent
    Added the path in routing.

--- src/app/guild/guild.routing.ts
+++ src/app/guild/guild.routing.ts
@@ -1,11 +1,16 @@
 import {RouterModule} from '@angular/router';
 import {GuildsComponent} from './guilds/guilds.component';
+import {GuildCreateComponent} from './guilds/guild-create/guild-create.component';
 
 export const guildRoutes = RouterModule.forRoot(
   [
     {
       path: 'guilds',
       component: GuildsComponent
+    },
+    {
+      path: 'guild/create',
+      component: GuildCreateComponent
     }
   ]
 );
