258abfbab3dc8fcbf34fbcc282bcf0115338b909
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2017-11-13 09:27:30 +0100

    Able to show guilds in guildWork view and select a guild

--- src/app/guild-work/guild-work/guild-work.component.ts
+++ src/app/guild-work/guild-work/guild-work.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {Guild} from '../../guild/shared/guild.model';
 import {Volunteer} from '../../volunteer/shared/volunteer.model';
 import {VolunteerService} from '../../volunteer/shared/volunteer.service';
+import {GuildService} from "../../guild/shared/guild.service";
 
 @Component({
   selector: 'app-guild-work',
@@ -16,22 +17,35 @@ export class GuildWorkComponent implements OnInit {
   endMoment: Date = new Date();
 
   volunteer: Volunteer;
-  selectedRow: number;
+  selectedVolunteerRow: number;
 
-  constructor(private volunteerService: VolunteerService) { }
+  guild: Guild;
+  selectedGuildRow: number;
+
+  constructor(private volunteerService: VolunteerService, private guildService: GuildService) { }
 
   ngOnInit() {
     this.guilds = [];
     this.volunteers = [];
     this.updateVolunteerList();
+    this.updateGuildList();
   }
 
   updateVolunteerList() {
     this.volunteerService.get().subscribe(volunteers => this.volunteers = volunteers);
   }
 
+  updateGuildList() {
+    this.guildService.get().subscribe(guilds => this.guilds = guilds);
+  }
+
   selectVolunteer(volunteer: Volunteer, index: number) {
     this.volunteer = volunteer;
-    this.selectedRow = index;
+    this.selectedVolunteerRow = index;
   }
+
+  selectGuild(guild: Guild, index: number) {
+    this.guild = guild;
+    this.selectedGuildRow = index;
+}
 }
